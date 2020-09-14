3dba0c2835d04217d099529a04522b5d080cff80
guthember <guthember@gmail.com>
2020-09-14 13:04:24 +0200

    Egyediek()

--- A01GuthErik/Program.cs
+++ A01GuthErik/Program.cs
@@ -37,10 +37,28 @@ namespace A01GuthErik
             return tomb[maxHely];
         }
 
+        static int Egyediek()
+        {
+            StreamWriter file = new StreamWriter("egyediek.txt");
+            int db = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if ((tomb[i] % 5 == 0) && (tomb[i] % 7 == 0))
+                {
+                    db++;
+                    file.WriteLine(tomb[i].ToString());
+                }
+            }
+            file.Close();
+
+            return db;
+        }
+
         static void Main(string[] args)
         {
             Beolvasas();
             Console.WriteLine("A maximum: {0}", Maximumertek());
+            Console.WriteLine("5 és 7 oszthatóak száma: {0}", Egyediek());
 
             Console.ReadKey();
         }
