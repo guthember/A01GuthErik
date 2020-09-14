8d51f943587fe47b997f0de9aae8f089e9349b59
guthember <guthember@gmail.com>
2020-09-14 12:56:24 +0200

    Beolvasas()

--- A01GuthErik/Program.cs
+++ A01GuthErik/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace A01GuthErik
 {
     class Program
     {
+        static int[] tomb = new int[1000];
+
+        static void Beolvasas()
+        {
+            StreamReader file = new StreamReader("adatok.dat");
+
+            
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = int.Parse(file.ReadLine()) * 2;
+            }
+
+            file.Close();
+        }
+
         static void Main(string[] args)
         {
+            Beolvasas();
+
+            Console.ReadKey();
         }
     }
 }
